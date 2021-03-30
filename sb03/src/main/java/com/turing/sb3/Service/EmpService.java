280438b9f65789ac01e7a03b88798bdbe411e0ff
pnf <pnf@qq.com>
2021-03-30 17:24:04 +0800

    ；；

--- sb03/src/main/java/com/turing/sb3/Service/EmpService.java
+++
@@ -1,7 +0,0 @@
-package com.turing.sb3.Service;
-
-import com.turing.sb3.entity.Emp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpService extends JpaRepository<Emp,Integer> {
-}
